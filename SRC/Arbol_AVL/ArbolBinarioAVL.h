4f1d4e131cec935a6bb1abff1b5f5ecd5900d4b1
brangarcor05 <brandon.garcrod1980@gmail.com>
2025-03-30 15:14:01 -0500

    Update ArbolBinarioAVL.h

--- SRC/Arbol_AVL/ArbolBinarioAVL.h
+++ SRC/Arbol_AVL/ArbolBinarioAVL.h
@@ -2,32 +2,44 @@
 #define ARBOLBINARIOAVL_H
 
 #include "NodoBinarioAVL.h"
+#include <algorithm>
+
 template <class T>
-
-class ArbolBinarioAVL{
-	protected:
-		NodoBinarioAVL<T> *raiz;
-	public:
-		ArbolBinarioAVL();
-		~ArbolBinarioAVL();
-		void setRaiz(NodoBinarioAVL<T>* raiz);
-		NodoBinarioAVL<T>* getRaiz();
-		bool esVacio();
-		T& datoRaiz();
-		int altura(NodoBinarioAVL<T> *inicio);
-		int tamano(NodoBinarioAVL<T> *inicio);
-		NodoBinarioAVL<T>* giroDerecha(NodoBinarioAVL<T>*& inicio);
-		NodoBinarioAVL<T>* giroIzquierdaDerecha(NodoBinarioAVL<T>* &padre);
-		NodoBinarioAVL<T>* giroIzquierda(NodoBinarioAVL<T>*& inicio);
-		NodoBinarioAVL<T>* giroDerechaIzquierda(NodoBinarioAVL<T>* &padre);
-		bool insertar(T& val);
-		bool eliminar(T& val);
-		bool buscar(T& val);
-		void preOrden(NodoBinarioAVL<T> *inicio);
-		void inOrden(NodoBinarioAVL<T> *inicio);
-		void posOrden(NodoBinarioAVL<T> *inicio);
-		void nivelOrden(NodoBinarioAVL<T> *inicio);
+class ArbolBinarioAVL {
+    protected:
+        NodoBinarioAVL<T>* raiz;
+    
+    public:
+        ArbolBinarioAVL();
+        ~ArbolBinarioAVL();
+        
+        void setRaiz(NodoBinarioAVL<T>* raiz);
+        NodoBinarioAVL<T>* getRaiz();
+        bool esVacio();
+        T& datoRaiz();
+        
+        int altura(NodoBinarioAVL<T>* nodo);
+        int tamano(NodoBinarioAVL<T>* nodo);
+        
+        NodoBinarioAVL<T>* balancear(NodoBinarioAVL<T>* nodo);
+        NodoBinarioAVL<T>* giroDerecha(NodoBinarioAVL<T>*& nodo);
+        NodoBinarioAVL<T>* giroIzquierda(NodoBinarioAVL<T>*& nodo);
+        NodoBinarioAVL<T>* giroIzquierdaDerecha(NodoBinarioAVL<T>*& nodo);
+        NodoBinarioAVL<T>* giroDerechaIzquierda(NodoBinarioAVL<T>*& nodo);
+        
+        NodoBinarioAVL<T>* insertarRec(NodoBinarioAVL<T>* nodo, const T& val);
+        bool insertar(T& val);
+        
+        NodoBinarioAVL<T>* eliminarRec(NodoBinarioAVL<T>* nodo, const T& val);
+        bool eliminar(T& val);
+        
+        bool buscar(T& val);
+        void preOrden(NodoBinarioAVL<T>* nodo);
+        void inOrden(NodoBinarioAVL<T>* nodo);
+        void posOrden(NodoBinarioAVL<T>* nodo);
+        void nivelOrden(NodoBinarioAVL<T>* nodo);
 };
-#include "ArbolBinarioAVL.hxx"
 
-#endif //ARBOLBINARIOAVL_H
+#include "ArbolBinarioAVL.cpp"
+
+#endif // ARBOLBINARIOAVL_H
