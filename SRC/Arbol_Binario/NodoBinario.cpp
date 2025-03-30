bba74b613120da65b83052442dea0efcc365391d
brangarcor05 <brandon.garcrod1980@gmail.com>
2025-03-30 17:52:06 -0500

    Update NodoBinario.cpp

--- SRC/Arbol_Binario/NodoBinario.cpp
+++ SRC/Arbol_Binario/NodoBinario.cpp
@@ -1,219 +1,41 @@
 #include "NodoBinario.h"
-#include <bits/stdc++.h>
 
-using namespace std;
+// Constructor con dato
+template <typename T>
+NodoBinario<T>::NodoBinario(T valor) : dato(valor), izq(nullptr), der(nullptr) {}
 
-template<class T>
-NodoBinario<T>::NodoBinario()
-{
-    this->hijoIzq=NULL;
-    this->hijoDer=NULL;
+// Constructor vacío
+template <typename T>
+NodoBinario<T>::NodoBinario() : izq(nullptr), der(nullptr) {}
+
+// Getters
+template <typename T>
+T NodoBinario<T>::obtenerDato() {
+    return dato;
 }
 
-template<class T>
-T& NodoBinario<T>::obtenerDato()
-{
-    return this->dato;
+template <typename T>
+NodoBinario<T>* NodoBinario<T>::obtenerHijoIzq() {
+    return izq;
 }
 
-template<class T>
-void NodoBinario<T>::fijarDato(T& val)
-{
-    this->dato = val;
+template <typename T>
+NodoBinario<T>* NodoBinario<T>::obtenerHijoDer() {
+    return der;
 }
 
-template<class T>
-NodoBinario<T>* NodoBinario<T>::obtenerHijoIzq()
-{
-    return this->hijoIzq;
+// Setters
+template <typename T>
+void NodoBinario<T>::fijarDato(T val) {
+    dato = val;
 }
 
-template<class T>
-NodoBinario<T>* NodoBinario<T>::obtenerHijoDer()
-{
-    return this->hijoDer;
+template <typename T>
+void NodoBinario<T>::fijarHijoIzq(NodoBinario<T>* izq) {
+    this->izq = izq;
 }
 
-template<class T>
-void NodoBinario<T>::fijarHijoIzq(NodoBinario<T> *izq)
-{
-    this->hijoIzq = izq;
+template <typename T>
+void NodoBinario<T>::fijarHijoDer(NodoBinario<T>* der) {
+    this->der = der;
 }
-
-template<class T>
-void NodoBinario<T>::fijarHijoDer(NodoBinario<T> *der)
-{
-    this->hijoDer = der;
-}
-
-template<class T>
-int NodoBinario<T>::altura()
-{
-	if(this->hijoIzq==NULL && this->hijoDer == NULL)
-	{
-		return 1;
-	}
-	if(this->hijoIzq==NULL)
-		return this->hijoDer->altura();
-	else if(this->hijoDer==NULL)
-		return this->hijoIzq->altura();
-
-
-	return max(this->hijoIzq->altura() , this->hijoDer->altura());
-}
-
-
-template<class T>
-int NodoBinario<T>::tamano()
-{
-	if(this->hijoIzq==NULL && this->hijoDer == NULL)
-	{
-		return 1;
-	}
-	if(this->hijoIzq==NULL)
-		return this->hijoDer->tamano() +1 ;
-	else if(this->hijoDer==NULL)
-		return this->hijoIzq->tamano() +1 ;
-
-
-	return this->hijoIzq->tamano() + this->hijoDer->tamano() +1 ;
-}
-
-template<class T>
-void NodoBinario<T>:: insertar(T& val)
-{
-
-	if(val > this->obtenerDato()){
-		if(this->hijoDer==NULL)
-		{
-			NodoBinario* nuevo= new NodoBinario();
-			nuevo->fijarDato(val);
-			this->fijarHijoDer(nuevo);
-		}
-		else
-		{
-			this->hijoDer->insertar(val);
-		}
-	}
-
-	if(val < this->obtenerDato()){
-		if(this->hijoIzq==NULL)
-		{
-			NodoBinario* nuevo= new NodoBinario();
-			nuevo->fijarDato(val);
-			this->fijarHijoIzq(nuevo);
-		}
-		else
-		{
-			this->hijoIzq->insertar(val);
-		}
-	}
-
-}
-
-
-template<class T>
-NodoBinario<T>* NodoBinario<T>:: buscar(T& val)
-{
-	if(this->obtenerDato()==val){
-		return this;
-	}
-
-	if(val < this-> obtenerDato()){
-        if(this->hijoIzq != NULL)
-            return this->hijoIzq->buscar(val);
-        else
-            return NULL;
-	}
-	else{
-        if(this->hijoDer != NULL)
-            return this->hijoDer->buscar(val);
-        else
-            return NULL;
-	}
-}
-
-
-template<class T>
-NodoBinario<T>* NodoBinario<T>::extremo_der()
-{
-    if(this->hijoDer != NULL)
-        return this->hijoDer->extremoDer();
-    else
-        return this;
-}
-
-template<class T>
-NodoBinario<T>* NodoBinario<T>::extremo_izq()
-{
-    if(this->hijoIzq != NULL)
-        return this->hijoIzq->extremo_izq();
-    else
-        return this;
-}
-
-
-template<class T>
-void NodoBinario<T>:: preOrden()
-{
-	cout << "\t"<<this->obtenerDato() << endl;
-	if(this->hijoIzq != NULL)
-		this->hijoIzq->preOrden();
-	if(this->hijoDer != NULL)
-		this->hijoDer->preOrden();
-	return;
-}
-
-
-template<class T>
-void NodoBinario<T>:: inOrden()
-{
-	if(this->hijoIzq != NULL)
-		this->hijoIzq->inOrden();
-
-	cout << "\t"<<this->obtenerDato() << endl;
-
-	if(this->hijoDer != NULL)
-		this->hijoDer->inOrden();
-	return;
-
-}
-
-
-
-template<class T>
-void NodoBinario<T>:: posOrden()
-{
-	if(this->hijoIzq != NULL)
-		this->hijoIzq->posOrden();
-
-	if(this->hijoDer != NULL)
-		this->hijoDer->posOrden();
-
-	cout << "\t"<<this->obtenerDato() << endl;
-	return;
-
-}
-
-
-template<class T>
-void NodoBinario<T>:: nivelOrden()
-{
-	queue<NodoBinario*> cola;
-	cola.push(this);
-	while(!cola.empty()){
-		cout<<"\t"<<cola.front()->obtenerDato()<<endl;
-		if(cola.front()->hijoIzq != NULL){
-			cola.push(cola.front()->hijoIzq);
-		}
-
-		if(cola.front()->hijoDer != NULL){
-			cola.push(cola.front()->hijoDer);
-		}
-		cola.pop();
-	}
-}
-
-
-
-
