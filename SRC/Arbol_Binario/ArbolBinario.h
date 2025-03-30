8767751bd840f68176a4b3f3fad64e0a8ec9a801
brangarcor05 <brandon.garcrod1980@gmail.com>
2025-03-30 17:52:36 -0500

    Update ArbolBinario.h

--- SRC/Arbol_Binario/ArbolBinario.h
+++ SRC/Arbol_Binario/ArbolBinario.h
@@ -1,28 +1,44 @@
-#ifndef __ARBOLBINARIO__H__
-#define __ARBOLBINARIO__H__
+#ifndef ARBOLBINARIOG_H
+#define ARBOLBINARIOG_H
 
 #include "NodoBinario.h"
 
-using namespace std;
+template <typename T>
+class ArbolBinarioG {
+private:
+    NodoBinario<T>* raiz;
 
-template<class T>
-class ArbolBinario {
-  protected:
-    NodoBinario<T> *raiz;
+    // Métodos auxiliares para recursión
+    void preOrden(NodoBinario<T>* nodo);
+    void inOrden(NodoBinario<T>* nodo);
+    void posOrden(NodoBinario<T>* nodo);
+    int altura(NodoBinario<T>* nodo);
+    int tamano(NodoBinario<T>* nodo);
+    NodoBinario<T>* insertar(NodoBinario<T>* nodo, T valor);
+    NodoBinario<T>* eliminar(NodoBinario<T>* nodo, T valor);
+    NodoBinario<T>* buscarMin(NodoBinario<T>* nodo);
+
 public:
-	ArbolBinario();
+    // Constructor
+    ArbolBinarioG();
+
+    // Métodos principales
+    NodoBinario<T>* getRaiz();
     bool esVacio();
-    T& datoRaiz();
+    T datoRaiz();
     int altura();
     int tamano();
-    void insertar(T& val);
-    bool eliminar(T& val);
-    NodoBinario<T>*  buscar(T& val);
+    void insertar(T valor);
+    void eliminar(T valor);
+    NodoBinario<T>* buscar(T valor);
+    NodoBinario<T>* buscarE(T valor);
+    
+    // Recorridos
     void preOrden();
     void inOrden();
     void posOrden();
-    void nivelOrden();
 };
 
-#include "ArbolBinario.hxx"
+#include "ArbolBinarioG.cpp" // Implementación
+
 #endif
