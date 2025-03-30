862b457ee8e670c8ec5f358f4e8105eccc7cad24
brangarcor05 <brandon.garcrod1980@gmail.com>
2025-03-30 15:40:02 -0500

    Update NodoBinarioAVL.h

--- SRC/Arbol_AVL/NodoBinarioAVL.h
+++ SRC/Arbol_AVL/NodoBinarioAVL.h
@@ -1,12 +1,14 @@
 #ifndef NODOBINARIOAVL_H
 #define NODOBINARIOAVL_H
 
+// Estructura que representa un nodo de un árbol AVL
 struct Nodo {
-    int clave;
-    Nodo* izquierda;
-    Nodo* derecha;
-    int altura;
+    int clave;                // Valor clave del nodo
+    Nodo* izquierda;          // Puntero al hijo izquierdo
+    Nodo* derecha;            // Puntero al hijo derecho
+    int altura;               // Altura del nodo en el árbol
 
+    // Constructor que inicializa el nodo con una clave y establece los hijos y la altura
     Nodo(int k) : clave(k), izquierda(nullptr), derecha(nullptr), altura(1) {}
 };
 
