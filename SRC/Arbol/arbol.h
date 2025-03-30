99937147cf10e2fb9ae9c8df43e3d7d6f730ba8f
brangarcor05 <brandon.garcrod1980@gmail.com>
2025-03-30 17:46:04 -0500

    Update arbol.h

--- SRC/Arbol/arbol.h
+++ SRC/Arbol/arbol.h
@@ -3,20 +3,43 @@
 
 #include "Nodo.h"
 
+// Definición de la plantilla de clase Arbol
 template <class T>
 class Arbol {
 public:
-    Nodo<T>* raiz;
+    Nodo<T>* raiz;  // Puntero a la raíz del árbol
+
+    // Constructor por defecto
     Arbol();
+
+    // Constructor que inicializa el árbol con un valor
     Arbol(T val);
+
+    // Verifica si el árbol está vacío
     bool esVacio();
+
+    // Obtiene la raíz del árbol
     Nodo<T>* obtenerRaiz();
+
+    // Fija la raíz del árbol
     void fijarRaiz(Nodo<T>* nraiz);
+
+    // Inserta un nodo en el árbol
     bool insertarNodo(T padre, T n);
+
+    // Elimina un nodo del árbol
     bool eliminarNodo(T n);
+
+    // Busca un nodo en el árbol
     Nodo<T>* buscarNodo(T val);
+
+    // Obtiene la altura del árbol
     int altura();
+
+    // Obtiene el tamaño del árbol
     int tamano();
+
+    // Imprime el árbol en orden de nivel
     void nivelOrden();
 };
 
