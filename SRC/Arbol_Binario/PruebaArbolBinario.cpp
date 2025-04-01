231b0440683dd903ba09049b6ad63d2ddf52f49a
brangarcor05 <brandon.garcrod1980@gmail.com>
2025-04-01 18:04:55 -0500

    Update PruebaArbolBinario.cpp

--- SRC/Arbol_Binario/PruebaArbolBinario.cpp
+++ SRC/Arbol_Binario/PruebaArbolBinario.cpp
@@ -1,46 +1,38 @@
-#include <iostream>
-#include "ArbolBinarioG.h"
-
+#include "ArbolBinario.h"
 using namespace std;
-
 int main() {
-    // Crear el árbol
-    ArbolBinarioG<int> arbol;
+    ArbolBinario<int> arbol;
 
-    // Insertar valores
     arbol.insertar(10);
     arbol.insertar(5);
+    arbol.insertar(20);
     arbol.insertar(15);
+    arbol.insertar(8);
     arbol.insertar(3);
-    arbol.insertar(7);
-    arbol.insertar(12);
-    arbol.insertar(18);
+    arbol.insertar(25);
 
-    // Mostrar recorridos del árbol completo
-    cout << "Recorrido Preorden del Árbol: ";
+    cout << "Raiz: " << arbol.datoRaiz() << endl;
+    cout << "Altura del árbol: " << arbol.altura() << endl;
+    cout << "Tamaño del árbol: " << arbol.tamano() << endl;
+
+    cout << "Recorrido Preorden: ";
     arbol.preOrden();
 
-    cout << "Recorrido Inorden del Árbol: ";
+    cout << "Recorrido Inorden: ";
     arbol.inOrden();
 
-    cout << "Recorrido Postorden del Árbol: ";
+    cout << "Recorrido Posorden: ";
     arbol.posOrden();
 
-    cout << "Recorrido Nivel Orden del Árbol: ";
-    arbol.nivelOrden();
+    cout << "Buscando el valor 15: ";
+    if (arbol.buscar(15)) cout << "Encontrado" << endl;
+    else cout << "No encontrado" << endl;
 
-    // Buscar un nodo y recorrer su subárbol
-    int valorBusqueda = 5;
-    NodoBinario<int>* subarbol = arbol.buscar(valorBusqueda);
-    if (subarbol) {
-        cout << "\nRecorridos del subárbol con raíz en " << valorBusqueda << ":\n";
-        cout << "Preorden: "; arbol.preOrden(subarbol);
-        cout << "Inorden: "; arbol.inOrden(subarbol);
-        cout << "Postorden: "; arbol.posOrden(subarbol);
-        cout << "Nivel Orden: "; arbol.nivelOrden(subarbol);
-    } else {
-        cout << "\nValor " << valorBusqueda << " no encontrado en el árbol.\n";
-    }
+    arbol.eliminar(15);
+    cout << "Después de eliminar 15:" << endl;
+
+    cout << "Recorrido Inorden: ";
+    arbol.inOrden();
 
     return 0;
 }
