55ae33cca48209cf84ffc10e9faf9dd3ed06d1fe
brangarcor05 <brandon.garcrod1980@gmail.com>
2025-04-01 17:17:56 -0500

    Update and rename ArbolBinarioAVL.h to AVLTree.h

---
+++ SRC/Arbol_AVL/AVLTree.h
@@ -0,0 +1,38 @@
+#ifndef AVLTREE_H
+#define AVLTREE_H
+
+#include "Nodo.h"
+#include <iostream>
+using namespace std;
+
+class AVLTree {
+private:
+    Nodo* raiz;
+
+    int altura(Nodo* n);
+    int balance(Nodo* n);
+    
+    Nodo* rotarDerecha(Nodo* y);
+    Nodo* rotarIzquierda(Nodo* x);
+    
+    Nodo* insertar(Nodo* nodo, int dato);
+    Nodo* eliminar(Nodo* nodo, int dato);
+    
+    Nodo* nodoMinimo(Nodo* nodo);
+    
+    void inOrden(Nodo* nodo);
+    void preOrden(Nodo* nodo);
+    void posOrden(Nodo* nodo);
+
+public:
+    AVLTree();
+    ~AVLTree();
+    void insertar(int dato);
+    void eliminar(int dato);
+    
+    void inOrden();
+    void preOrden();
+    void posOrden();
+};
+
+#endif
