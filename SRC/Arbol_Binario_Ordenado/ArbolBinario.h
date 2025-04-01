45030131cd759b403304672f444c54cb894fe623
brangarcor05 <brandon.garcrod1980@gmail.com>
2025-04-01 10:53:58 -0500

    Update ArbolBinario.h

--- SRC/Arbol_Binario_Ordenado/ArbolBinario.h
+++ SRC/Arbol_Binario_Ordenado/ArbolBinario.h
@@ -1,25 +1,33 @@
-#ifndef ARBOLBINARIO_H
-#define ARBOLBINARIO_H
+#ifndef ARBOL_BINARIO_H
+#define ARBOL_BINARIO_H
 
-template <class T>
+#include "NodoBinario.h"
+#include <iostream>
 
-class ArbolBinario{
-	protected:
-		NodoBinario<T> *raiz;
-	public:
-		ArbolBinario();
-		~ArbolBinario();
-		bool esVacio();
-		T& datoRaiz();
-		int altura(NodoBinario<T> *inicio);
-		int tamano(NodoBinario<T> *inicio);
-		bool insertar(T& val,NodoBinario<T> *nodo);
-		bool eliminar(T& val);
-		bool buscar(T& val);
-		void preOrden(NodoBinario<T> *inicio);
-		void inOrden(NodoBinario<T> *inicio);
-		void posOrden(NodoBinario<T> *inicio);
-		void nivelOrden(NodoBinario<T> *inicio);
+template <typename T>
+class ArbolBinario {
+private:
+    NodoBinario<T>* raiz;
+    void preOrden(NodoBinario<T>* nodo);
+    void inOrden(NodoBinario<T>* nodo);
+    void posOrden(NodoBinario<T>* nodo);
+    void destruir(NodoBinario<T>* nodo);
+    
+public:
+    ArbolBinario();
+    ~ArbolBinario();
+
+    NodoBinario<T>* getRaiz();
+    bool esVacio();
+    T datoRaiz();
+    
+    void insertar(T valor);
+    bool buscar(T valor);
+    void eliminar(T valor);
+
+    void preOrden();
+    void inOrden();
+    void posOrden();
 };
 
-#endif //ARBOLBINARIO_H
+#endif
